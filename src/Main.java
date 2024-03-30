da9c991c5edbc8f981a02a5a56022f2e75b037e5
NataliaDuenas <nduenass@unal.edu.co>
2024-03-30 11:03:13 -0500

    funciona pero Time Limit Exceed

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,46 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     ListaDoblementeEnlazada TextoMalo;
     ListaDoblementeEnlazada TextoTemporal;
     String Linea;
+
     Main(String Linea) {
         TextoMalo = new ListaDoblementeEnlazada();
         TextoTemporal = new ListaDoblementeEnlazada();
         TextoMalo.insertarAlInicio(TextoTemporal);
         char accionprevia=']';
-        char home='[';
-        char back=']';
-
         for (int i = 0; i < Linea.length(); i++) {
             char caracter = Linea.charAt(i);
-
-            if (caracter != home && caracter != back) {
+            char home='[';
+            char back=']';
+            if ((caracter != home && caracter != back)) {
                 TextoTemporal.insertarAlFinal(caracter);
             } else if(accionprevia==home){
                 TextoMalo.insertarAlInicio(TextoTemporal);
                 TextoTemporal.vaciar();
                 accionprevia=caracter;
-            }else if(accionprevia==back){
+            } else if(accionprevia==back){
                 TextoMalo.insertarAlFinal(TextoTemporal);
                 TextoTemporal.vaciar();
                 accionprevia=caracter;
             }
-            
-            if (i==Linea.length()-1) {
-                if(accionprevia==home){
-                    TextoMalo.insertarAlInicio(TextoTemporal);
-                    TextoTemporal.vaciar();
-                    accionprevia=caracter;
-                }else if(accionprevia==back){
-                    TextoMalo.insertarAlFinal(TextoTemporal);
-                    TextoTemporal.vaciar();
-                    accionprevia=caracter;
-                }
+            if (i == Linea.length() - 1) {
+                TextoMalo.insertarAlFinal(TextoTemporal);
             }            
         }
-        
-    System.out.println(TextoMalo.toString());
+        System.out.println(TextoMalo.toString());
     }
-
 
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       //long startTime = System.currentTimeMillis();
        String Linea1 = sc.nextLine();
-       String Linea2 = sc.nextLine();
-       sc.close();
        new Main(Linea1);
+       String Linea2 = sc.nextLine();
        new Main(Linea2);
-      /*  long endTime = System.currentTimeMillis();
-       long duration = endTime - startTime;
-       System.out.println("Tiempo de ejecución: " + duration + " milisegundos"); */
-       
+       sc.close();
     }
+    
     static class Nodo {
         char dato;
         Nodo siguiente;
@@ -81,32 +65,25 @@ public class Main{
         public boolean estaVacia() {
             return cabeza == null;
         }
-
+        
         public void insertarAlInicio(ListaDoblementeEnlazada nuevaLista) {
-           if (estaVacia()) {
-            cabeza = nuevaLista.cabeza;
-            cola = nuevaLista.cola;
-
-            }else {
+            if (nuevaLista.estaVacia()) {
+                return;
+            }
+            if (estaVacia()) {
+                cabeza = nuevaLista.cabeza;
+                cola = nuevaLista.cola;
+            } else {
+                nuevaLista.cola.siguiente = cabeza;
                 cabeza.anterior = nuevaLista.cola;
-                nuevaLista.cola.siguiente =cabeza;
                 cabeza = nuevaLista.cabeza;
             }
-           
-           
         }
-        public void insertarAlInicio(char dato) {
-            Nodo nuevo = new Nodo(dato);
-            if (estaVacia()) {
-                cabeza = nuevo;
-                cola = nuevo;
-            } else {
-                nuevo.siguiente = cabeza;
-                cabeza.anterior = nuevo;
-                cabeza = nuevo;
-            }
-        }
+        
         public void insertarAlFinal(ListaDoblementeEnlazada nuevaLista) {
+            if (nuevaLista.estaVacia()) {
+                return;
+            }
             if (estaVacia()) {
                 cabeza = nuevaLista.cabeza;
                 cola = nuevaLista.cola;
@@ -114,7 +91,6 @@ public class Main{
                 cola.siguiente = nuevaLista.cabeza;
                 nuevaLista.cabeza.anterior = cola;
                 cola = nuevaLista.cola;
-
             }
         }
 
@@ -129,32 +105,20 @@ public class Main{
                 cola = nuevo;
             }
         }
-        
+
         public void vaciar() {
             cabeza = null;
-            cola = null; 
+            cola = null;
         }
-
-        /* public String toString(){
+        
+        public String toString(){
             Nodo nodo = cabeza;
             String texto="";
             while (nodo!= null) {
                 texto += nodo.dato;
                 nodo = nodo.siguiente;
             }
-
             return texto;
-        }     
-        
-        */
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            Nodo actual = cabeza;
-            while (actual != null) {
-                sb.append(actual.dato);
-                actual = actual.siguiente;
-            }
-            return sb.toString();
-        }    
-    } 
+        }
+    }
 }
